323cf63f58fc5e164e96b1e0226db97bf0045a31
deepaknagar2231 <deepakumarcm2231@gmail.com>
2024-10-20 12:06:35 +0530

    first step done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState(0);
+  const hanldeChange = (e) => {
+    setValue(e.target.value);
+  }
+  const handleClick = () => {
+    const newString = value.replace(/^"(.+)"$/, '$1');
+    calculateStringValue(newString)
+  }
+  const calculateStringValue = (val) => {
+    if (val === "") setResult(0);
+    else {
+      let arr = val.split(",");
+      let res = parseInt(arr[0]);
+      for (let i = 1; i < arr.length; i++) {
+        res += parseInt(arr[i]);
+      }
+      setValue("");
+      setResult(res);
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='calculator'>
+        <input value={value} onChange={hanldeChange} placeholder='Enter the string' />
+        <button onClick={handleClick}>Add the number</button>
+        {`Result of string is: ${result}`}
+      </div>
     </div>
   );
 }
